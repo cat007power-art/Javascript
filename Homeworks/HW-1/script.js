438866d030db15475159ca0328dced4ebfedd16f
cat007power-art <cat007power@gmail.com>
2025-10-29 15:45:56 +0300

    завершено

---
+++ Homeworks/HW-1/script.js
@@ -0,0 +1,49 @@
+let s1 =`hello`;
+let s2=`owu`;
+let s3=`com`;
+let s4=`ua`;
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(s4);
+
+let n1=1;
+let n2=10;
+let n3=-999;
+let n4=123;
+let n5=3.14;
+let n6=2.7;
+let n7=16;
+
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(n4);
+console.log(n5);
+console.log(n6);
+console.log(n7);
+
+let b1=true;
+let b2=false;
+
+console.log(b1);
+console.log(b2);
+
+let firstName=`Вікторія`;
+let middleName=`Олександрівна`;
+let lastName=`Волошина`;
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+
+
+
