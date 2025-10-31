8c1d5fef3db97f2c03ada1d54057e4ce5f747b4a
cat007power-art <cat007power@gmail.com>
2025-10-31 16:06:06 +0300

    завершено

---
+++ Homeworks/HW-2-4/script.js
@@ -0,0 +1,22 @@
+let users=[
+    {name: 'name-1', userName: 'A', password: '12'},
+    {name: 'name-2', userName: 'B', password: '23'},
+    {name: 'name-3', userName: 'C', password: '34'},
+    {name: 'name-4', userName: 'D', password: '45'},
+    {name: 'name-5', userName: 'E', password: '56'},
+    {name: 'name-6', userName: 'F', password: '67'},
+    {name: 'name-7', userName: 'G', password: '78'},
+    {name: 'name-8', userName: 'H', password: '89'},
+    {name: 'name-9', userName: 'I', password: '91'},
+    {name: 'name-10', userName: 'J', password: '11'},
+];
+console.log(users[0].password);
+console.log(users[1].password);
+console.log(users[2].password);
+console.log(users[3].password);
+console.log(users[4].password);
+console.log(users[5].password);
+console.log(users[6].password);
+console.log(users[7].password);
+console.log(users[8].password);
+console.log(users[9].password);
