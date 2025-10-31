a8aa77c5a22305a79da91c9a150b97ef3d3df701
cat007power-art <cat007power@gmail.com>
2025-10-31 20:46:29 +0300

    завершено

--- Homeworks/HW-2-11/script.js
+++ Homeworks/HW-2-11/script.js
@@ -1,6 +1,6 @@
 let x = 0;
-if(x === 0 || x === null || x === undefined || x ==== NaN || x === '') {
-    x='default';
+if(x === 0 || x === null || x === undefined || x === NaN || x === '') {
+    x = 'default';
 }
 
 let x = 0;
