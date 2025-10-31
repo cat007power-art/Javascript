2961dd22a88a66332080c7994f8b5db0015c5d42
cat007power-art <cat007power@gmail.com>
2025-10-31 16:52:58 +0300

    завершено

---
+++ Homeworks/HW-2-5/script.js
@@ -0,0 +1,35 @@
+let temps=[
+   [8,15,11],
+   [10,17,13],
+   [15,21,18],
+   [14,20,17],
+   [20,26,24],
+   [15,20,17],
+   [16,24,10]
+]
+
+let temps=[
+    {day: 'monday', values:[8,15,11]},
+    {day: 'tuesday', values:[10,17,13]},
+    {day: 'wednesday', values:[15,24,18]},
+    {day: 'thursday', values:[14,20,17]},
+    {day: 'friday', values:[20,26,24]},
+    {day: 'saturday', values:[15,20,14]},
+    {day: 'sunday', values:[16,24,10]},
+]
+
+
+let temps=[
+    {day: 'monday', morningTemp:8,dayTemp:15, eveningTemp:11},
+    {day: 'tuesday', morningTemp:10,dayTemp:17, eveningTemp:13},
+    {day: 'wednesday', morningTemp:15,dayTemp:24, eveningTemp:18},
+    {day: 'thursday', morningTemp:14,dayTemp:20, eveningTemp:17},
+    {day: 'friday', morningTemp:20,dayTemp:26, eveningTemp:24},
+    {day: 'saturday', morningTemp:15,dayTemp:20, eveningTemp:14},
+    {day: 'sunday', morningTemp:1,dayTemp:24, eveningTemp:10},
+
+
+
+
+
+]
