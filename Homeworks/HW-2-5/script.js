a9e66220cc5a828735dbb9a77f6bf77efb02d06f
cat007power-art <cat007power@gmail.com>
2025-10-31 16:53:49 +0300

    завершено

--- Homeworks/HW-2-5/script.js
+++ Homeworks/HW-2-5/script.js
@@ -1,22 +1,22 @@
-let temps=[
-   [8,15,11],
-   [10,17,13],
-   [15,21,18],
-   [14,20,17],
-   [20,26,24],
-   [15,20,17],
-   [16,24,10]
-]
+// let temps=[
+//    [8,15,11],
+//    [10,17,13],
+//    [15,21,18],
+//    [14,20,17],
+//    [20,26,24],
+//    [15,20,17],
+//    [16,24,10]
+// ]
 
-let temps=[
-    {day: 'monday', values:[8,15,11]},
-    {day: 'tuesday', values:[10,17,13]},
-    {day: 'wednesday', values:[15,24,18]},
-    {day: 'thursday', values:[14,20,17]},
-    {day: 'friday', values:[20,26,24]},
-    {day: 'saturday', values:[15,20,14]},
-    {day: 'sunday', values:[16,24,10]},
-]
+// let temps=[
+//     {day: 'monday', values:[8,15,11]},
+//     {day: 'tuesday', values:[10,17,13]},
+//     {day: 'wednesday', values:[15,24,18]},
+//     {day: 'thursday', values:[14,20,17]},
+//     {day: 'friday', values:[20,26,24]},
+//     {day: 'saturday', values:[15,20,14]},
+//     {day: 'sunday', values:[16,24,10]},
+// ]
 
 
 let temps=[
@@ -27,9 +27,4 @@ let temps=[
     {day: 'friday', morningTemp:20,dayTemp:26, eveningTemp:24},
     {day: 'saturday', morningTemp:15,dayTemp:20, eveningTemp:14},
     {day: 'sunday', morningTemp:1,dayTemp:24, eveningTemp:10},
-
-
-
-
-
 ]
