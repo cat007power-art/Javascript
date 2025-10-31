6f02d1f5042f8d0e3360b5ad32aa9e84b5b52c86
cat007power-art <cat007power@gmail.com>
2025-10-31 15:45:12 +0300

    завершено

---
+++ Homeworks/HW-2-3/script.js
@@ -0,0 +1,20 @@
+let book1={
+    title:"abc",
+    pageCount:100,
+    genre: 'fantastic',
+    authors:[{name:"John", age:30}, {name:"Anthony", age:55}]
+}
+
+let book2={
+    title:"def",
+    pageCount:200,
+    genre: 'detective',
+    authors:[{name:"Oleg", age:40}, {name:"Vasya", age:29}]
+}
+
+let book3={
+    title:"ghi",
+    pageCount:300,
+    genre: 'novel',
+    authors:[{name:"Olga", age:44}, {name:"Valera", age:34}]
+}
