9e0a467427a374dce1dd5210735d611629e9a6f9
cat007power-art <cat007power@gmail.com>
2025-11-20 13:57:06 +0300

    завершено

---
+++ Homeworks/HW-6/HW-6-3/script.js
@@ -0,0 +1,11 @@
+// let s='HELLO WORLD';
+// const s1=s.toLowerCase();
+// console.log(s1);
+
+// let s='LOREM IPSUM';
+// const s1=s.toLowerCase();
+// console.log(s1);
+
+let s='JAVASCRIPT IS COOL';
+const s1=s.toLowerCase();
+console.log(s1);
