2fa8bbc85852bfbdd6194394297218a263a1c80e
cat007power-art <cat007power@gmail.com>
2025-11-20 13:37:49 +0300

    завершено

---
+++ Homeworks/HW-6/HW-6-2/script.js
@@ -0,0 +1,11 @@
+// let s= 'hello world';
+// const s1 = s.toUpperCase();
+// console.log(s1);
+
+// let s= 'lorem ipsum';
+// const s2 = s.toUpperCase();
+// console.log(s2);
+
+let s= 'javascript is cool';
+const s3 = s.toUpperCase();
+console.log(s3);
