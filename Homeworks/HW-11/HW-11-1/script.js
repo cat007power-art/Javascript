1988c3cb28420dca4531ad833d780618a49b05cf
cat007power-art <cat007power@gmail.com>
2026-02-03 16:25:43 +0300

    завершено

---
+++ Homeworks/HW-11/HW-11-1/script.js
@@ -0,0 +1,55 @@
+const cartsDiv = document.getElementById('carts');
+
+fetch('https://dummyjson.com/carts')
+    .then(res => res.json())
+    .then(cartsObject => {
+        const { carts } = cartsObject;
+
+        for (const cart of carts) {
+            const div = document.createElement('div');
+            div.classList.add('cart-container');
+
+            const divWithInfo = document.createElement('div');
+            divWithInfo.innerText = `
+        "total": ${cart.total},
+        "discountedTotal": ${cart.discountedTotal},
+        "userId": ${cart.userId},
+        "totalProducts": ${cart.totalProducts},
+        "totalQuantity": ${cart.totalQuantity}
+      `;
+
+
+            const ol = document.createElement('ol');
+            for (const product of cart.products) {
+                const li = document.createElement('li');
+
+                const info = document.createElement('p');
+                info.innerText = `
+          "id": ${product.id},
+          "title": ${product.title},
+          "price": ${product.price},
+          "quantity": ${product.quantity},
+          "total": ${product.total},
+          "discountPercentage": ${product.discountPercentage},
+          "discountedTotal": ${product.discountedTotal}
+        `;
+
+                const img = document.createElement('img');
+                img.src = product.thumbnail;
+                img.alt = product.title;
+
+                li.appendChild(info);
+                li.appendChild(img);
+                ol.appendChild(li);
+            }
+
+            div.appendChild(divWithInfo);
+            div.appendChild(ol);
+            cartsDiv.appendChild(div);
+        }
+    })
+
+
+
+
+
